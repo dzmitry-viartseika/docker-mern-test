8812c05f11204858c1183581a2661f566bdaf19e
wertey <verteyko1990@gmail.com>
2021-09-12 16:35:12 +0300

    added docker files

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT;
+const host = process.env.HOST;
+
 app.get('/test', (req, res) => {
     res.send('Our api server is working correctly');
 })
 
-app.listen(3000, () => {
-    console.log('Started api server');
+app.listen(port, () => {
+    console.log('Started api server on port', port);
+    console.log('Started api server on host', host);
 })
