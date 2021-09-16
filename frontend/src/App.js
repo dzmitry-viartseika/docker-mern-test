886cfb472392b0a9c9f5d7f1499c6342e7d570f7
wertey <verteyko1990@gmail.com>
2021-09-16 23:01:52 +0300

    added frontend production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Wertey 2222 wertey
+          Edit <code>src/App.js</code> and save to reload. Wertey 2222 wertey23232 wwww test
         </p>
         <a
           className="App-link"
